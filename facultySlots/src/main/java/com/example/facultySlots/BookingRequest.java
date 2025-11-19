beed0b23054db39c89e6feb610d75e9321efaf30
Vraj Shah <122716046+vyom1307@users.noreply.github.com>
2025-11-19 21:41:47 +0530

    Remove unnecessary fields from BookingRequest
    Removed duration, startTime, and endTime fields from BookingRequest.

--- facultySlots/src/main/java/com/example/facultySlots/BookingRequest.java
+++ facultySlots/src/main/java/com/example/facultySlots/BookingRequest.java
@@ -7,7 +7,5 @@ public class BookingRequest {
     private String date;      // e.g., "2025-11-25"
     private String slotId;    // e.g., "10:00-10:30"
     private String studentUid; // Unsecured: Coming from request body
-    private Integer duration;
-    private String startTime;
-    private String endTime;
+    
 }
